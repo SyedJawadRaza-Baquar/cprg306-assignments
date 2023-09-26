ae99dc989d7f12cb7779964483d30f0e1f48055e
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-09-25 21:30:36 -0600

    made some changes!

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -1,10 +1,12 @@
-export default StudentInfo() {
+import Link from 'next/link'
+
+export default function StudentInfo() {
     return (
         <>
             <h1>My Information</h1>
             <p>Name: Syed Jawad Raza Baquar</p>
             <p>Course Section: CPRG 306 B</p>
-            <Link href="">My GitHub repository</Link>
+            <Link href="https://github.com/SyedJawadRaza-Baquar/cprg306-assignments">My GitHub repository</Link>
         </>
-    )
-}
+    );
+    }
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../studentInfo"
+
+export default function Page() {
+    return (
+        <>
+        <h1>My Shopping List</h1>
+        <StudentInfo />
+        </>
+    );
+    }
