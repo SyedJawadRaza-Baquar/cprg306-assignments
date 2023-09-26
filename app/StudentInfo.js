49680a07ed7630dda88de4f1be7153eaadb8c4dc
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-09-25 18:38:31 -0600

    Create StudentInfo.js
    wrote some code!

---
+++ app/StudentInfo.js
@@ -0,0 +1,10 @@
+export default StudentInfo() {
+    return (
+        <>
+            <h1>My Information</h1>
+            <p>Name: Syed Jawad Raza Baquar</p>
+            <p>Course Section: CPRG 306 B</p>
+            <Link href="">My GitHub repository</Link>
+        </>
+    )
+}
