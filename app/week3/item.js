24fade267c9f49af2a326c3affc5fdef8d442540
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-10-02 22:49:43 -0600

    added css

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,6 +1,6 @@
 export default function Item({item}) {
     return (
-        <ul>
+        <ul className="px-4 py-2 m-8 text-red-400 text-base text-center font-normal bg-slate-800 rounded hover:bg-blue-600 border-blue-800">
             <li>{item.name}</li>
             <li>{item.quantity}</li>
             <li>{item.category}</li>
--- app/week3/page.js
+++ app/week3/page.js
@@ -3,8 +3,8 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <>
-        <main>
-            <h1>Shopping List</h1>
+        <main className="px-4 py-2 m-4 text-red-600 text-xl text-center font-bold bg-gray-400 rounded border-blue-800">
+            <h1 >Shopping List</h1>
             <ItemList />
         </main>
         </>
