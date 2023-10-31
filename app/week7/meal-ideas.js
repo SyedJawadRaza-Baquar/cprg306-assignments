760a46b57e9a1bbca14faf42b9c98ed4ffe3924a
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-10-30 23:53:06 -0600

    week-7 assignment

---
+++ app/week7/meal-ideas.js
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const fetchMealIdeas = async (ingredient) => {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+        const data = await response.json();
+        console.log(data);    
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export default function MealIdeas({ingredient}) {
+    const [meals, setMeals] = useState([]);
+}
---
+++ app/week7/page.js
@@ -0,0 +1,24 @@
+"use client" ;
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import { useState } from "react";
+
+export default function Page() {
+
+    const [items, setItems] = useState(itemsData);
+
+    const handleAddItem = (item) => {
+        setItems([...items, item]);
+    }
+
+    return (
+        <>
+        <main className="flex-direction: column w-screen h-screen px-4 py-2 m-4 text-red-600 text-xl text-center font-bold bg-gray-400 rounded border-blue-800">
+            <h1 >Shopping List</h1>
+            <ItemList items={items} />
+            <NewItem onAddItem={handleAddItem} />
+        </main>
+        </>
+    );
+}
